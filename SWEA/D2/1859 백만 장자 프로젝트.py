731a248ae73ea7f3693659e06a84300e7cf9c280
ssooh-i <101916058+ssooh-i@users.noreply.github.com>
2022-05-18 04:24:05 +0900

    enter delete

--- SWEA/D2/1859 백만 장자 프로젝트.py
+++ SWEA/D2/1859 백만 장자 프로젝트.py
@@ -1,7 +1,44 @@
+# T = int(input())
+# for tc in range(1, T+1):
+#     N = int(input())
+#     nums = list(map(int, input().split(" ")))
+#     max_n = max(nums) #list 증 최대값 추출
+#     index = nums.index(max_n) #list 인덱스 번호 추출
+#     print(f"#{tc} ", end ="")
+#     sum = 0
+#     for i in range(0, N):
+#         if nums[i] == max_n:
+#             sum += 0
+#             if nums[i] < max_n:
+#                 sum += (max_n - nums[i])
+#         else:
+#                 sum += (max_n - nums[i])
+#     print(sum)
+    #     if nums[i] < max_n:
+    #         #print(nums[i], max_n)
+    #         sum += (max_n - nums[i])
+    #         #print(sum)
+    #         #sum += (max_n - nums[i])
+    # # print(sum)
+    # #         for j in range(i+1, N):
+    # #             sum += (max_n-nums[j])
+    # #             print("1", sum)
+    #     elif nums[i] == max_n:
+    #         sum += 0
+    # print(sum)
+    # # print("2",sum)
+
+#역순으로 풀기
 T = int(input())
-for tc in range(1, T+1):
+for t in range(1,T+1):
     N = int(input())
-    nums = list(map(int, input().split(" ")))
-    max_n = max(nums) #list 증 최대값 추출
-    index = nums.index(max_n) #list 인덱스 번호 추출
-    print(index)
+    nums = list(map(int,input().split()))
+    max_n = nums[-1]
+    #print(max_n)
+    sum = 0
+    for i in range(len(nums)-1,-1,-1): #역순으로 가지고 오기
+        if max_n > nums[i]:
+            sum += max_n-nums[i]
+        else: # max_n =< nums[i] 경우
+            max_n = nums[i]
+    print(f"#{t} {sum}")
