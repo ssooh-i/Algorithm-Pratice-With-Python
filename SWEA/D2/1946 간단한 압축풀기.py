21ffa1aba2f088104a29226a440c5c15d8ff96c2
ssooh-i <101916058+ssooh-i@users.noreply.github.com>
2022-05-21 00:43:03 +0900

    Empty Message

---
+++ SWEA/D2/1946 간단한 압축풀기.py
@@ -0,0 +1,61 @@
+T = int(input())
+for tc in range(1, T + 1):
+    n = int(input())
+
+    all = ''
+    for i in range(0, n):
+        char, _loop = input().split(" ")
+        for _ in range(int(_loop)):
+            all += char
+
+    # i = 0
+    # while i<len(all):
+    #     print(all[i],end="")
+    #     i+=1
+    #     if i%10 == 0:
+    #         print()
+
+    answer = ""
+    for i in range(0, len(all)):
+        if (i != 0) & (i % 10 == 0):
+            answer += '\n'
+        answer += all[i]
+
+    print(answer.strip())
+
+    # i = 10
+    # while i<len(all):
+    #     print(all[i-10:i])
+    #     i+=10
+    # print(all[i-10:])
+
+    # for j in range(0, len(all)):
+    #     for i in range(0,10):
+    #         print(all[i], end="")
+    #     print()
+# T = int(input())
+# for tc in range(1, T+1):
+#     n = int(input())
+#     document = {}
+#     result = []
+#     all = []
+#     for i in range(0, n):
+#         s, num = input().split(" ")
+#         document[s] = int(num)
+#         #print(document) #딕셔너리에 값추가 확인
+#     _key = list(document.keys())
+#     _valuse = list(document.values())
+#     for j in range(0, n):
+#         result.append(_key[j]*_valuse[j])
+#     for i in range(0, len(result)):
+#         all += list(result[i])
+#     #print(all)
+#
+#     i = 0
+#
+#     print(f"#{tc} ")
+#     while i < len(all):
+#         print(all[i], end="")
+#         i += 1
+#         if i % 10 == 0:
+#             print()
--- SWEA/D2/2007 패턴 마디의 길이.py
+++ SWEA/D2/2007 패턴 마디의 길이.py
@@ -1,10 +1,33 @@
-#문제 케이스가 이상하여 백준 16229번 반복 패턴 문제로 대체함
-n,m = map(int, input().split(" "))
-s = list(input())
-if len(s) > n:
-    print('s input Error')
-else:
-    for j in range(0, m):
-        for i in range(len(s), len(s)+m):
-            s[i].append(s[j])
-            print(s)
+# #문제 케이스가 이상하여 백준 16229번 반복 패턴 문제로 대체함
+# n,m = map(int, input().split(" "))
+# s = list(input())
+# k = s[0]
+# if len(s) > n:
+#     print('s input Error')
+# else:
+#     for i in range(0, m):
+#         s.append(s[i])
+#         print(s)
+#
+    # for j in range(0, m):
+    #     for i in range(len(s), len(s)+m):
+    #         s[i].append(s[j])
+    #         print(s)
+
+T = int(input())
+
+for tc in range(1, T + 1):
+
+    text = input()
+    patten = []
+    next_patten = []
+    ans = 0
+    for i in range(11):  # 마디의 최대 길이가 10이므로 range(11)
+        patten = text[:i]  # patten리스트에 패턴 입력
+        next_patten = text[i:i * 2]  # 다음 패턴 입력
+        # print(patten)
+        # print(next_patten)
+        if i != 0 and patten == next_patten:  # 다음 패턴과 이번 패턴이 같은경우
+            ans = len(patten)  # 길이 출력
+            break
+    print(f'#{tc} {ans}')
