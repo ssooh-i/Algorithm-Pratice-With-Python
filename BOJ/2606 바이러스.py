90e59b1fce353c436c05fcd26cec6d3f1b4cbda9
ssooh-i <101916058+ssooh-i@users.noreply.github.com>
2022-05-25 20:46:55 +0900

    Empty Message

--- BOJ/2606 바이러스.py
+++ BOJ/2606 바이러스.py
@@ -15,17 +15,22 @@ for _ in range(m):
     nodes[_from].addEdge(_to)
     nodes[_to].addEdge(_from)
 
-visited = [0 for i in range(n)] #false로 안하고 0으로 한 이유는 다하고 나서 sum할꺼라서
+# for i in range(0, len(nodes)):
+#     print(nodes[i].edges)
+
+visited = [0 for i in range(n+1)] #false로 안하고 0으로 한 이유는 다하고 나서 sum할꺼라서
 
 
 def dfs(v):
-    visited[v] = 1 #방문하면 1로 바꾸기
-    for i in range(n+1):
-        if nodes[i][v] == 1 and visited[i] == 0:
+    for i in range(len(nodes[v].edges)):
+        if visited[nodes[v].edges[i]] == 0:
+            visited[nodes[v].edges[i]] = 1  # 방문하면 1로 바꾸기
             dfs(i)
             #print(dfs(i))
         #print(nodes[i][v],end=" ")
 
+visited[1] = 1
 dfs(1) #1번 컴퓨터에 연결된 것만 찾음
+print(visited)
 
 print(sum(visited)-1) #몇개인지 세고 시작점인 노드 1번을 삭제한 개수가 정답
