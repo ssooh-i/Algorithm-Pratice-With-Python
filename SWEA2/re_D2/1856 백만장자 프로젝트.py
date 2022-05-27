0115066050ed9e663fc3b4f5d7ed478a2339d730
ssooh-i <101916058+ssooh-i@users.noreply.github.com>
2022-05-27 23:09:51 +0900

    Empty Message

--- SWEA2/re_D2/1856 백만장자 프로젝트.py
+++ SWEA2/re_D2/1856 백만장자 프로젝트.py
@@ -1,6 +1,24 @@
 T = int(input())
 for tc in range(1, T+1):
+    n = int(input())
+    N = list(map(int, input().split()))
+    _sum = 0
+    _max = max(N)
+    for i in range(n-1, -1, -1):
+        if _max > N[i]:
+            _sum += _max - N[i]
+    print(f"#{tc} {_sum}")
+
+T = int(input())
+for t in range(1,T+1):
     N = int(input())
-    arr=[]
-    for i in range(N):
-        arr.append(list(map(int, input().split())))
+    nums = list(map(int,input().split()))
+    max_n = nums[-1]
+    #print(max_n)
+    sum = 0
+    for i in range(len(nums)-1,-1,-1): #역순으로 가지고 오기
+        if max_n > nums[i]:
+            sum += max_n-nums[i]
+        else: # max_n =< nums[i] 경우
+            max_n = nums[i]
+    print(f"#{t} {sum}")
