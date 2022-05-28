ab4b91705056575942baaf67ff3d65f72bf8ff11
ssooh-i <101916058+ssooh-i@users.noreply.github.com>
2022-05-28 18:20:25 +0900

    Empty Message

--- SWEA2/re_D2/1954 달팽이 숫자.py
+++ SWEA2/re_D2/1954 달팽이 숫자.py
@@ -1,15 +1,40 @@
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N = int(input())
     matrix = [[0 for _ in range(N)] for _ in range(N)]
+    now_x = 0  # 현재 위치
+    now_y = 0
+    # 동 남 서 북
+    dy = [0, 1, 0, -1]  # y축
+    dx = [1, 0, -1, 0]  # x축
+    D = 0
 
-    # 우, 하, 왼, 상
-    dy = [0, 1, 0, -1] # y축방향
-    dx = [1, 0, -1, 0] # x축방향
+    for i in range(1, N * N + 1):
+        matrix[now_y][now_x] = i
 
-    for i in range(N):
-        for j in range(N):
-            '''
-            0이 아닌 숫자라면 방향 전환
-            0이라면 숫자 추가
-            '''
+        next_y = now_y + dy[D]
+        next_x = now_x + dx[D]
+
+        if (next_y >= N) or (next_x >= N):
+            if D == 3:
+                D = 0
+            else:
+                D += 1
+        elif (next_y < 0) or (next_x < 0):
+            if D == 3:
+                D = 0
+            else:
+                D += 1
+        elif matrix[next_y][next_x] != 0:
+            if D == 3:
+                D = 0
+            else:
+                D += 1
+
+        now_y += dy[D]
+        now_x += dx[D]
+    print(f"#{tc}")
+    for i in range(0, N):
+        for j in range(0, N):
+            print(matrix[i][j], end=" ")
+        print()
---
+++ SWEA2/re_D2/1961 숫자배열회전.py
@@ -0,0 +1,26 @@
+def rotate90(arr):
+    arr2 = [[0 for _ in range(N)] for _ in range(N)]
+    for y in range(0, N):
+        for x in range(0, N):
+            arr2[x][N-1-y] = arr[y][x]
+    return arr2
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = []
+    for _ in range(N):
+        arr.append(list(map(int, input().split())))
+    arr2 = rotate90(arr)
+    arr3 = rotate90(arr2)
+    arr4 = rotate90(arr3)
+
+    total = [arr2, arr3, arr4]
+    print(f"#{tc}")
+    for r in range(0, N):
+        for i in range(0, 3):
+            for c in range(0, N):
+                print(total[i][r][c], end = "")
+            print(end=" ")
+        print()
