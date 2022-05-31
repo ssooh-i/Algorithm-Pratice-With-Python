bd3423c15f3a3232f4aac874bd7bde3f839081dc
ssooh-i <101916058+ssooh-i@users.noreply.github.com>
2022-05-31 22:40:08 +0900

    Empty Message

--- programmers/완주하지 못한 선수.py
+++ programmers/완주하지 못한 선수.py
@@ -1,11 +1,20 @@
-def solution(participant, completion):
-    answer = []
-    lst1= participant.split()
-    lst2 = completion.split()
-    complement = list(set(lst1) - set(lst2))
-    print(complement)  # ['B', 'A']
-    complement = list(set(lst1).difference(lst2))
-    print(complement)
-    return answer
+# def solution(participant, completion):
+#     answer = []
+#     lst1= participant.split()
+#     lst2 = completion.split()
+#     complement = list(set(lst1) - set(lst2))
+#     print(complement)  # ['B', 'A']
+#     complement = list(set(lst1).difference(lst2))
+#     print(complement)
+#     return answer
+#
+# print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
 
-print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+
+    for part, com in zip(participant, completion):
+        if part != com:
+            return part
+    return participant[-1]
