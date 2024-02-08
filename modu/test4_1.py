f8e83ae075fa2d58667bee3717a798ab17a351ae
ssooh-i <615ksh@gmail.com>
2024-02-09 02:03:05 +0900

    재귀함수 연습 4-1, 책 정답 추가

--- modu/test4_1.py
+++ modu/test4_1.py
@@ -1,6 +1,16 @@
+#내가 푼 거
 def sum(n):
     if n <= 1:
         return 1
     return n + sum(n-1)
 
-print(sum(10))
+print(sum(100))
+
+#책 정답
+def sum_n(n):
+    if n == 0:
+        return 0
+    return n + sum_n(n-1)
+
+print()
+print(sum(100))
