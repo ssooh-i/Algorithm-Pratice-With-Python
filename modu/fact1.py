d243682887e8b45c35de18ae720f04f37ce99b37
ssooh-i <615ksh@gmail.com>
2024-02-09 01:39:26 +0900

    factorial ver2

--- modu/fact1.py
+++ modu/fact1.py
@@ -1,7 +1,16 @@
-def fact(n):
-    facto = 1;
-    for i in range(1,n+1):
-        facto = facto*i
-    return facto
+# 팩토리얼 버전 1, 일반 반복문
+# def fact(n):
+#     facto = 1;
+#     for i in range(1,n+1):
+#         facto = facto*i
+#     return facto
+#
+# print(fact(10))
 
-print(fact(10))
+#팩토리얼 버전 2, 재귀함수
+def fact2(n):
+    if n <= 1:
+        return 1
+    return n * fact2(n - 1)
+
+print(fact2(5))
