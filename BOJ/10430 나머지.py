3d82268896b5f995653b7968bb84c7ec53ffbf6a
ssooh-i <615ksh@gmail.com>
2024-02-17 00:07:00 +0900

    나머지(조건을 잘보자)

--- BOJ/10430 나머지.py
+++ BOJ/10430 나머지.py
@@ -1,4 +1,9 @@
-if ((A+B)%C) and ((A%C)+(B%C)%C:
-
-
-A, B, C = map(int, input().split())
+num = list(map(int, input().split()))
+A = num[0]
+B = num[1]
+C = num[2]
+#0과 1로 결과를 나누는 게 아니라 그냥 나머지를 고르는 거 였다
+print((A + B) % C)
+print(((A % C) + (B % C)) % C)
+print((A * B) % C)
+print(((A % C) * (B % C)) % C)
