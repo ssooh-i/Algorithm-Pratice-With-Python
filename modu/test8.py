55af161d964d88af624d01cb27275992585277d7
ssooh-i <615ksh@gmail.com>
2024-02-21 20:37:27 +0900

    선택 정렬 - 최솟값 인덱스 찾기, 오름차순으로 정렬된 리스트 만들기

--- modu/test8.py
+++ modu/test8.py
@@ -14,7 +14,7 @@
 #
 #     return min_idx
 
-def find_min_idx2(a): #책 코드
+def find_min_idx2(a):  # 책 코드
     n = len(a)
     min_idx = 0
     for i in range(1, n):
@@ -23,8 +23,16 @@ def find_min_idx2(a): #책 코드
 
     return min_idx
 
+def sel_sort(a):
+    result = []  # 새 리스트
+    while a:  # 주어진 리스트에 값이 남아 있는 동안 계속 실행
+        min_idx = find_min_idx2(a)  # 리스트의 남아있는 값중 최소값의 위치
+        result.append(a.pop(min_idx)) # 찾은 최소값을 빼내어 새 리스트에 저장
+    return result
 
-#d = [2, 4, 5, 1, 3]
+# d = [2, 4, 5, 1, 3]
+# print(find_min_idx(d))
+
 v = [2, 3, 4, 5, 1]
-#print(find_min_idx(d))
 print(find_min_idx2(v))
+print(sel_sort(v))
