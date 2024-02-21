d51143b2a69014232c5bd690e872fb6cb1fb5284
ssooh-i <615ksh@gmail.com>
2024-02-21 21:36:11 +0900

    삽입 정렬 - 오름차순, 내림차순

--- modu/test9_1.py
+++ modu/test9_1.py
@@ -14,15 +14,13 @@ def ins_sort(a):
 
 def ins_resort(a):
     n = len(a)
-    for i in range(1, n):  # 1번부터 n-1까지
-        key = a[i]  # i번 위치에 있는 값을 key에 저장
-        # j를 i 바로 왼쪽 위치로 저장
+    for i in range(1, n):
+        key = a[i]
         j = i - 1
-        # 리스트의 j번 위치에 있는 값과 key를 비교해 key가 삽입될 적절한 위치를 찾음
-        while j >= 0 and a[j] < key:
-            a[j + 1] = a[j]  # 삽입할 공간이 생기도록 값을 오른쪽으로 한칸 이동시킴
+        while j >= 0 and a[j] < key: #부등호 바꾸면 내림차순이 됨
+            a[j + 1] = a[j]
             j -= 1
-        a[j + 1] = key  # 찾은 삽입 위치에 key를 저장
+        a[j + 1] = key
 
 d = [2, 4, 5, 1, 3]
 # ins_sort(d)
