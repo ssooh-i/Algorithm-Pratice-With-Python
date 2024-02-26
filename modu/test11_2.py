52dfdc37b87ffed6fa7c5e8745efbe516e4748b1
ssooh-i <615ksh@gmail.com>
2024-02-26 15:01:27 +0900

    퀵 정렬

--- modu/test11_2.py
+++ modu/test11_2.py
@@ -14,19 +14,19 @@ def quick_sort_sub(a, start, end):
 
     for j in range(start, end):
         if a[j] <= pivot:
-            print('pivot : ', pivot)
-            print('바뀌기 전 a[i] = ', a[i], ', a[j] = ', a[j])
-            print('바뀌기 전 d:', d)
+            # print('pivot : ', pivot)
+            # print('바뀌기 전 a[i] = ', a[i], ', a[j] = ', a[j])
+            # print('바뀌기 전 d:', d)
             a[i], a[j] = a[j], a[i]
-            print('바뀐 후 d:', d)
+            # print('바뀐 후 d:', d)
             i += 1
-    print('for문 끝남 a[i] = ', a[i], ', a[end] = ', a[end])
+    # print('for문 끝남 a[i] = ', a[i], ', a[end] = ', a[end])
     # 왜 끝에 요소와 i번째 요소를 바꿔주는 가?
     # 피벗을 기준으로 리스트를 두 부분으로 나누고 각각 정렬하는 것이 퀵 정렬의 핵심이기 때문에
     # 피벗을 중간으로 이동 시켜주는 것이다.
     a[i], a[end] = a[end], a[i]
-    print('for문 끝나고 d : ', d)
-    print()
+    # print('for문 끝나고 d : ', d)
+    # print()
 
     # 재귀호출
     quick_sort_sub(a, start, i - 1)  # 기준 값보다 작은 그룹을 재귀 호출로 다시 정렬
