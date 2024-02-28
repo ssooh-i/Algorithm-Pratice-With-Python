1c11072de70ba6a9b3f8ec67cba345d81a97ac0a
ssooh-i <615ksh@gmail.com>
2024-02-28 18:57:03 +0900

    최대 수익 알고리즘 - 가능한 모든 경우 비교하기

---
+++ modu/test18.py
@@ -0,0 +1,18 @@
+# 최대 수익 알고리즘
+# 방법 1 : 가능한 모든 경우를 비교하기
+def max_profit(prices):
+    n = len(prices)
+    max_profit = 0  # 최대 수익은 항상 0 이상
+
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            # i날에 사서 j날에 팔았을 때 얻는 수익
+            if prices[j] - prices[i] > max_profit:  # 최대 수익보다 크면 최대 수익 갱신
+                max_profit = prices[j] - prices[i]
+
+    return max_profit
+
+
+stock = [10300, 9600, 9800, 8200, 7800, 8300, 9500, 9800, 10200, 9500]
+
+print(max_profit(stock))
