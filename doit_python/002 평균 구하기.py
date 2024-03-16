61e81b14052c805ca1dd93158121a1b43350d2b2
ssooh-i <615ksh@gmail.com>
2024-03-16 17:21:27 +0900

    백준 1546번

--- doit_python/002 평균 구하기.py
+++ doit_python/002 평균 구하기.py
@@ -1,1 +1,16 @@
 # 백준 1546
+n = int(input())
+a = list(map(int, input().split()))
+max = 0
+
+# 최대값 찾기
+for i in a:
+    if max < i:
+        max = i
+
+# 새로운 점수 구하기
+new_sum = 0
+for i in a:
+    new_sum += (i/max)*100
+
+print(new_sum/n)
