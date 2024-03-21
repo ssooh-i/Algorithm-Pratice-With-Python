b08e7f94c3f995d4417c1b2f805ac381967192e5
ssooh-i <615ksh@gmail.com>
2024-03-21 12:12:38 +0900

    004-구간 합 구하기2

---
+++ doit_python/004 구간 합 구하기 2.py
@@ -0,0 +1,14 @@
+# 백준 11660번
+# 표의 크기 n (n*n배열준비), 합을 구해야하는 횟수 m
+# 표에 삽입할 데이터 (0부터 아니고 1부터 인덱스 생각해주기)
+# m개의 줄에 x1 y1 x2 y2가 주어짐
+# x1 y1부터 x2,y2까지의 합구하기
+
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+mapA = [n][n]
+
+for i in range(n):
+    pass
